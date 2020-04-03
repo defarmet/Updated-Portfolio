21fcd318886cc9dcba1e0ba2337330f84920ddeb
defarmet <defarmet@protonmail.com>
2020-04-02 23:29:36 -0400

    Added more projects and a sticky navbar

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,12 +1,15 @@
-function load_project(name, link, src) {
+function load_project(name, link) {
 	var project = $("<div>").addClass("col-md-6");
-	project.append($("<img>").addClass("card-img").attr("src", src));
-	var project_name = $("<div>").addClass("project_name");
-	var a = $("<a>").addClass("text-reset text-decoration-none");
-	a.attr("href", link).text(name);
-	project_name.append(a);
-	project.append(project_name);
+	var src = "assets/images/" + name + ".png";
+	var img = $("<img>").addClass("card-img").attr("src", src);
+	var project_name = $("<div>").text(name);
+	project_name.addClass("w-100 text-center position-relative project");
+	var a = $("<a>").addClass("text-decoration-none");
+	a.attr("href", link).append(img, project_name);
+	project.append(a);
 	$(".card-body").append(project);
 }
 
-load_project("Portfolio", "index.html", "assets/images/Portfolio.png");
+load_project("Portfolio", "index.html");
+load_project("Giftastic", "https://defarmet.github.io/Giftastic/");
+load_project("RPS-Multiplayer", "https://defarmet.github.io/RPS-Multiplayer/");
